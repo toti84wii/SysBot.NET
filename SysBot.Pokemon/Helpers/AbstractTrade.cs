362749c12616c7fd04256356e8a829f62821a9c5
Easy World <easy_world_118@Hotmail.com>
2023-09-07 23:16:14 +0800

    fix: compile error

--- SysBot.Pokemon/Helpers/AbstractTrade.cs
+++ SysBot.Pokemon/Helpers/AbstractTrade.cs
@@ -434,7 +434,7 @@ namespace SysBot.Pokemon.Helpers
 
             var la = new LegalityAnalysis(pk);
             var enc = la.EncounterMatch;
-            pk.CurrentFriendship = enc is EncounterStatic s ? s.EggCycles : pk.PersonalInfo.HatchCycles;
+            pk.CurrentFriendship = enc is IHatchCycle s ? s.EggCycles : pk.PersonalInfo.HatchCycles;
 
             Span<ushort> relearn = stackalloc ushort[4];
             la.GetSuggestedRelearnMoves(relearn, enc);
